ec2a3eb44104ffe0cc604ebcfd3d006d23c5ab76
Maurice Rogers <scsurfermaurice@gmail.com>
2023-10-19 19:03:09 -0700

    Change yrSalary to yrIncome and update JS

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-function budgetResults(yrSalary, monthlyExpenses){
-    console.log(`year salary is ${yrSalary}`);
+function budgetResults(yrIncome, monthlyExpenses){
+    console.log(`year income is ${yrIncome}`);
     console.log(`monthlyExpenses is ${monthlyExpenses}`);
-    const fundsAfterTaxes66Kto338K = yrSalary > 66296 && yrSalary < 338639? 
-                        yrSalary - Math.floor(((yrSalary-66295)*.093)+2918.91):
+    const fundsAfterTaxes66Kto338K = yrIncome > 66296 && yrIncome < 338639? 
+                        yrIncome - Math.floor(((yrIncome-66295)*.093)+2918.91):
                         "salaryOutOfRange";
-    const fundsAfterTaxes52Kto66K = yrSalary > 52000 && yrSalary < 66000? 
-                        yrSalary - Math.floor(((yrSalary-52455)*.08)+1811.71):
+    const fundsAfterTaxes52Kto66K = yrIncome > 52000 && yrIncome < 66000? 
+                        yrIncome - Math.floor(((yrIncome-52455)*.08)+1811.71):
                         "salaryOutOfRange";
-    const fundsAfterTaxes37Kto52K = yrSalary > 37000 && yrSalary < 52000? 
-                        yrSalary - Math.floor(((yrSalary-37788)*.06)+931.69):
+    const fundsAfterTaxes37Kto52K = yrIncome > 37000 && yrIncome < 52000? 
+                        yrIncome - Math.floor(((yrIncome-37788)*.06)+931.69):
                         "salaryOutOfRange";
     function chooseFundsAfterTaxesCalc(){
-        return yrSalary > 66296 && yrSalary < 338639? fundsAfterTaxes66Kto338K: yrSalary > 52000 && yrSalary < 66000? fundsAfterTaxes52Kto66K: yrSalary > 37000 && yrSalary < 52000? fundsAfterTaxes37Kto52K: 'update for budgetResults function is needed for this salary tax range';
+        return yrIncome > 66296 && yrIncome < 338639? fundsAfterTaxes66Kto338K: yrIncome > 52000 && yrIncome < 66000? fundsAfterTaxes52Kto66K: yrIncome > 37000 && yrIncome < 52000? fundsAfterTaxes37Kto52K: 'update for budgetResults function is needed for this salary tax range';
     }
     function calc_needs_wants_savingsOfFundsAfterTaxes(fundsAfterTaxes){
         return {
